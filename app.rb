feb5f1c602bc21ad82db8786ee5cc654f62f3ecf
Vasiliy Novosad <novosadvasiliy@gmail.com>
2016-07-04 12:36:46 +0300

    added login and commands

--- app.rb
+++ app.rb
@@ -2,24 +2,37 @@ require 'telegram/bot'
 require_relative 'quest_parser'
 
 token = '171556746:AAEd8YJrYhFsiLjVEkyIk2cmluEf2lWkA5s'
+parser = nil
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
+    chat_id = -24142491 # message.chat.id
     case message.text
       when '/start'
-        bot.api.sendMessage(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-      when '/start_quest'
-        parser = QuestParser.new('https://cloclo26.cldmail.ru/s5wznAqt9XFC98kfg2Q/G/634V/ZjAYVqBHJ?key=b73e6de189d6c2928dcb7fed0a20950b25425556', 'link')
+        # p message.chat
+        bot.api.sendMessage(chat_id: chat_id, text: "Hello, #{message.from.first_name}")
+      when /^\/start_/
+        # parser = QuestParser.new('http://zhitomir.quest.ua/gameengines/encounter/play/41977', 'link')
+        parser = QuestParser.new(message.text[7..-1], 'link')
         p parser.url
-        #bot.api.sendMessage(chat_id: message.chat.id, text: parser.url)
+        # bot.api.sendMessage(chat_id: chat_id, text: parser.url)
       when '/parse'
-        # if parser
+        if parser
           parser.get_html_from_url
           parser.parse_content
           if parser.level_name != parser.level_name_new
             parser.level_name = parser.level_name_new
-            bot.api.sendMessage(chat_id: message.chat.id, text: parser.level_name)
+            bot.api.sendMessage(chat_id: chat_id, text: parser.level_name)
           end
-        # end
+          parser.question_texts_new.each do |mess|
+            bot.api.sendMessage(chat_id: chat_id, text: mess)
+            parser.question_texts.push(mess)
+          end
+          parser.question_texts_new = []
+        end
+      when /^\./
+        p message.text[1..-1]
+        parser.get_html_from_url
+        parser.send_code(message.text[1..-1])
     end
   end
 end
--- quest_parser.rb
+++ quest_parser.rb
@@ -1,52 +1,79 @@
 require 'open-uri'
-require 'nokogiri'
+require 'mechanize'
+
+# http://zhitomir.quest.ua/gameengines/encounter/play/41977
+# http://quest.ua/Login.aspx
 
 class QuestParser
-  attr_accessor :level_name, :level_name_new, :question_headers, :question_headers_new, :question_texts, :question_texts_new, :url, :type_url, :doc
+  attr_accessor :page, :level_name, :level_name_new, :question_headers, :question_headers_new, :question_texts, :question_texts_new, :url, :type_url
 
   def initialize(page_url, url_type)
-    level_name = ''
-    question_headers = {}
-    question_texts = {}
-    url = page_url
-    type_url = url_type
+    @level_name = ''
+    @question_headers = []
+    @question_headers_new = []
+    @question_texts = []
+    @question_texts_new = []
+    @url = page_url
+    @type_url = url_type
+    @agent = Mechanize.new
   end
 
   def get_html_from_url
+
     if type_url == 'file'
-      doc = File.open(url) { |f| Nokogiri::HTML(f) } # Nokogiri::HTML(html)
+      @page = File.open(@url) { |f| Nokogiri::HTML(f) } # Nokogiri::HTML(html)
     else
-      doc = Nokogiri::HTML(open(url))
+      #@doc = Nokogiri::HTML(open(url))
+      @page = @agent.get(@url)
+      if need_log_in
+        login_form = @page.form
+        login_form.Login = 'vnovosad'
+        login_form.Password = 'V0rtex'
+        login_form.submit
+      end
+      @page = @agent.get(@url)
     end
   end
 
   def parse_content
-    content = doc.css('.content')
+    content = @page.search('.content')
     parse_level_name(content)
-    parse_question(content)
+    parse_questions(content)
+  end
+
+  def send_code(code)
+    code_form = @page.form
+    code_form['LevelAction.Answer'] = code
+    code_form.submit
+    parse_content
   end
 
   private
 
+  def need_log_in
+    @page.form.button_with(value: 'Вход')
+  end
+
   def parse_level_name(content)
     level = content.at_css('h2')
-    level_name_new = level.children.map { |c| c.name == 'span' ? c.children[0].text : c.text }.join
+    @level_name_new = level.children.map { |c| c.name == 'span' ? c.children[0].text : c.text }.join
   end
 
   def parse_questions(content)
-    question_headers = content.css('h3')
-    question_headers.each do |el|
+    @question_headers_new = []
+    @question_texts_new = []
+    question_headers_from_content = content.css('h3')
+    question_headers_from_content.each do |el|
       if el.attributes['class'].nil?
         question_header = el.text
         unless question_headers.include?(question_header)
-          question_headers_new.push(question_header)
-          question_headers.push(question_header)
+          @question_headers_new.push(question_header)
         end
       end
     end
 
-    question_texts = content.css('h3 + p')
-    question_texts.each do |el|
+    question_texts_from_content = content.css('h3 + p')
+    question_texts_from_content.each do |el|
       question_text = ''
       el.children.each do |row|
         case row.name
@@ -60,8 +87,7 @@ class QuestParser
       question_text.gsub!("\r", "\n")
       puts question_text
       unless question_texts.include?(question_text)
-        question_texts_new.push(question_text)
-        question_texts.push(question_text)
+        @question_texts_new.push(question_text)
       end
     end
 
