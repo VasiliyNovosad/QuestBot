c54b4d1e91193cc921920df58b782d929260c207
Vasyl Novosad <novosad.vasiliy@gmail.com>
2017-07-27 00:36:31 +0300

    small fixes

--- lib/quest_parser_json.rb
+++ lib/quest_parser_json.rb
@@ -9,12 +9,39 @@ class QuestParserJson
 
   attr_accessor :domain_name, :game_id, :login, :password, :cookie, :errors, :level_json
 
+  def initialize(domain_name, game_id)
+    @domain_name = domain_name
+    @game_id = game_id
+    @cookie = ''
+    @level_json = nil
+    @login = nil
+    @password = nil
+    @errors = []
+  end
+
   def get_html_from_url
     begin
-      get_level_json
+      self.level_json = get_level
     rescue
       return false
     end
+  end
+
+  def parse_content(with_q_time)
+    return if level_json.nil?
+
+    # content = @page.search('.content')
+    # if content
+    #   parse_level_name(content)
+    #   new_level = false
+    #   if @level_name != @level_name_new
+    #     @question_texts = []
+    #     @question_texts_new = []
+    #     @level_name = @level_name_new
+    #     new_level = true
+    #   end
+    #   parse_questions(content, with_q_time, new_level)
+    # end
   end
 
   private
@@ -28,41 +55,45 @@ class QuestParserJson
     response = http.request(request)
     data = JSON.parse response.body
     if data['Error'].zero?
-      all_cookies = response.get_fields('set-cookie')
-      cookies_array = []
-      all_cookies.each do |cookie|
-        cookies_array.push(cookie.split('; ')[0])
-      end
-      self.cookie = cookies_array.join('; ')
+      set_cookies(response)
     else
       self.errors = data['Message']
     end
   end
 
-  def get_level_json
+  def set_cookies(response)
+    all_cookies = response.get_fields('set-cookie')
+    cookies_array = []
+    all_cookies.each do |cookie|
+      cookies_array.push(cookie.split('; ')[0])
+    end
+    self.cookie = cookies_array.join('; ')
+  end
+
+  def get_level
+    response = get_level_response
+    if response.code == '200'
+       JSON.parse response.body
+    else
+      sign_in
+      unless errors.nil?
+        response = get_level_response
+        if response.code == '200'
+          JSON.parse response.body
+        else
+          self.errors = 'Помилка отримання даних'
+          nil
+        end
+      end
+    end
+  end
+
+  def get_level_response
     uri = URI.parse("http://#{domain_name}#{ENGINE_URL}#{game_id}#{FORMAT_URL}")
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Get.new(uri.request_uri)
     request['content-type'] = 'application/json'
-    request['Cookie'] = cookie
-    response = http.request request
-    if response.code == '200'
-      self.level_json = JSON.parse response.body
-    else
-      sign_in
-      unless errors.nil?
-        uri = URI.parse("http://#{domain_name}#{ENGINE_URL}#{game_id}#{FORMAT_URL}")
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Get.new(uri.request_uri)
-        request['content-type'] = 'application/json'
-        request['Cookie'] = cookie
-        response = http.request request
-        if response.code == '200'
-          self.level_json = JSON.parse response.body
-        else
-          self.errors = 'Помилка отримання даних'
-        end
-      end
-    end
+    request['Cookie'] = self.cookie
+    http.request request
   end
 end
