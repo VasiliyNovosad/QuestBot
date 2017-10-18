273c091d34a50df2216c892fd82e115a32d5cc86
Vasyl Novosad <vasiliy@eventsoft.com.ua>
2017-10-18 10:45:07 +0300

    changes settings to env config vars

--- lib/app_configurator.rb
+++ lib/app_configurator.rb
@@ -1,21 +1,7 @@
 require 'logger'
-require 'yaml'
 
 class AppConfigurator
-  def get_token
-    YAML::load(IO.read('config/secrets.yml'))['telegram_bot_token']
-  end
-
   def get_logger
     Logger.new(STDOUT, Logger::DEBUG)
   end
-
-  def self.get_personal_chat_id
-    YAML::load(IO.read('config/secrets.yml'))['personal_chat_id']
-  end
-
-  def self.get_admin_id
-    YAML::load(IO.read('config/secrets.yml'))['admin_id']
-  end
-
 end
